da6e7277d2f1864f0db6936cebb11a75a9669811
victshev <victshev259@gmail.com>
2023-08-22 17:06:49 +0400

    Tests added

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,23 @@
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testExactlyOneThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void testLessThanOneThousand() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+
+}
